8fa92dec32f110ade084255e168ec9bbce85042a
Bioshaun <ricekent@163.com>
2018-08-28 03:16:57 -0400

    rm rbase.py

--- reaflow/expression/rbase.py
+++
@@ -1,8 +0,0 @@
-from .base import expression_filter
-
-
-def extract_params(kwargs):
-    def decorate(func):
-        func_params = kwargs['params']
-        return func(**func_params)
-    return decorate
